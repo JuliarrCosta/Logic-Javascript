b7e46d325191a4da13710bfdd90ebbab79c8349d
JuliarrCosta <Juliaraquelrc@outlook.com>
2024-11-01 08:39:23 -0300

    Finalizando metodo média

--- Question4/Turma.js
+++ Question4/Turma.js
@@ -38,15 +38,32 @@ class Turma extends Aluno{
 
     //considerando que o front limitou a escolha do tipo da prova não vou validar a entrada
     assessmentStudent(matricula, tipoAvaliacao, valor){
-        const aluno = findStudent(matricula)
-        if(tipoAvaliacao === "p1"){
-            aluno.p1 = parseFloat(valor)
-        }
-        else if(tipoAvaliacao === "p2"){
-            aluno.p2 = parseFloat(valor)
+        const aluno = findStudent(matricula) 
+        if(typeof aluno !== undefined){
+            if(tipoAvaliacao === "p1"){
+                aluno.p1 = parseFloat(valor).toFixed(1)
+            }
+            else if(tipoAvaliacao === "p2"){
+                aluno.p2 = parseFloat(valor).toFixed(1)
+            }
         }
     }
 
-    
+    media(matricula){
+        let media = null
+        const aluno = findStudent(matricula)
 
+        if(typeof aluno !== undefined){
+            if(aluno.p1 !== null && aluno.p1 !== null ){
+                media = ((aluno.p1 + aluno.p2)/2).toFixed(1)
+            }else if(aluno.p1 !== null && aluno.p1 === null){
+                media = (aluno.p1/2).toFixed(1)
+            }else if(aluno.p2 !== null && aluno.p2 === null){
+                media = (aluno.p2/2).toFixed(1)
+            }else{
+                media = 0;
+            }  
+        }
+        return media;
+    }
 }
